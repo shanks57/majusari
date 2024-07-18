8eb69dc377db29379aa851bd58ff57686eb9a0c3
masaditya <aeka489@gmail.com>
2024-07-18 21:28:35 +0700

    feat(web-2) : add datatables

---
+++ app/Http/Controllers/EtalaseController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Showcase;
+
+class EtalaseController extends Controller
+{
+    public function index()
+    {
+        $etalases = Showcase::all();
+        return view('pages.master-showcases', compact('etalases'));
+    }
+}
+
